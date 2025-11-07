cd50bf05c5212d8f0ac982fe6450a4b7d9031126
Elivandro <Elivandro@hotmail.com.br>
2025-11-07 12:16:53 -0400

    docker file and deleted or renome some files

--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo "Olá Mundo!";
-echo "<br/>";
-echo "Eu estou rodando a partir de um container com nginx, php e mysql.";
-echo "<br/>";
-echo "coloque os arquivos dentro da pasta ./public";
